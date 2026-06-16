80bb8852950d91bf196275bf9072e9e0e3df225e
iBL3ND <48933084+IBL3ND@users.noreply.github.com>
2026-06-16 18:39:31 +0800

    更新 AAA.js

--- AAA.js
+++ AAA.js
@@ -1,11 +1,19 @@
+const url = $request.url;
+
+if (!$response.body) {
+  $done({});
+}
+
 let obj = JSON.parse($response.body);
 
-if (obj.data) {
-    obj.data.title = "iBL3ND";
-    obj.data.city_name = "iBL3ND";
+if (url.includes("/api/sns/v1/localfeed/header")) {
+  if (obj?.data) {
+    obj.data.title = "东京";
+    obj.data.city_name = "东京";
     obj.data.distance = 0;
+  }
 }
 
 $done({
-    body: JSON.stringify(obj)
+  body: JSON.stringify(obj)
 });
